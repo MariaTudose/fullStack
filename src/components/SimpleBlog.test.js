9362275c133a1de2ecd0fc4f58eb6342372ec9d5
Maria Tudose <maria.tudose@aalto.fi>
2020-01-03 00:07:49 +0200

    5.14: blogilistan testit, step2

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
 test('renders content', () => {
@@ -16,4 +16,24 @@ test('renders content', () => {
   expect(component.container).toHaveTextContent('Testing with jest')
   expect(component.container).toHaveTextContent('Cool dude')
   expect(component.container).toHaveTextContent('blog has 0 likes')
+})
+
+test('clicking the button twice calls event handler twice', async () => {
+  const blog = {
+    title: 'Testing with jest',
+    author: 'Cool dude',
+    likes: 0
+  }
+
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />
+  )
+
+  const button = getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
 })
