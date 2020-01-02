2f219af6200bd53228bd595013e5b50b6099dd2f
Maria Tudose <maria.tudose@aalto.fi>
2020-01-02 14:04:21 +0200

    5.6* blogilistan frontend, step6

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, {useState} from 'react'
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setShowDetails(!showDetails)}>
+        {blog.title} {blog.author}
+      </div>
+      {showDetails && (
+        <div>
+          <a href={blog.url}>{blog.url}</a>
+          <br/>
+          {blog.likes} likes 
+          <button>like</button>
+          <br/>
+          added by {blog.user.name}
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
