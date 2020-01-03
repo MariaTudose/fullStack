2ea70a259e9bacea519da47b9bfc983bc3570cb2
Maria Tudose <maria.tudose@aalto.fi>
2020-01-03 14:20:18 +0200

    5.18-5.20 blogilista ja hookit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import BlogForm from './components/BlogForm'
+import LoginForm from './components/LoginForm'
 import Notification from './components/Notification'
 import Togglable from './components/Togglable'
-import loginService from './services/login'
 import blogService from './services/blogs'
 
-const blogFields = { title: '', author: '', url: '' }
-
 function App() {
-  const [password, setPassword] = useState('')
-  const [username, setUsername] = useState('')
   const [user, setUser] = useState(null)
   const [blogs, setBlogs] = useState([])
-  const [newBlog, setNewBlog] = useState(blogFields)
   const [notification, setNotification] = useState({ msg: '', style: '' })
 
   useEffect(() => {
@@ -32,67 +27,15 @@ function App() {
     }
   }, [])
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
-    try {
-      const user = await loginService.login({
-        username, password,
-      })
-
-      window.localStorage.setItem('user', JSON.stringify(user))
-      blogService.setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (e) {
-      setNotificationMsg('wrong credentials', 'error')
-    }
-  }
-
   const handleLogout = () => {
     setUser(null)
     window.localStorage.removeItem('user')
     blogService.setToken('')
   }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  )
+  const saveUser = user => setUser(user)
 
-  const addBlog = e => {
-    e.preventDefault()
-    blogService
-      .create(newBlog)
-      .then(data => {
-        setBlogs(blogs.concat(data))
-        setNewBlog(blogFields)
-        setNotificationMsg(`A new blog ${data.title} by ${data.author} added`, 'success')
-      })
-  }
-
-  const handleBlogChange = ({ value, name }) => {
-    setNewBlog({ ...newBlog, [name]: value })
-  }
+  const addBlog = blog => setBlogs(blogs.concat(blog))
 
   const setNotificationMsg = (msg, style) => {
     setNotification({ msg, style })
@@ -128,10 +71,7 @@ function App() {
     <div>
       <Notification msg={notification.msg} style={notification.style} />
       {(user === null) ? (
-        <div>
-          <h2>Log in to application</h2>
-          {loginForm()}
-        </div>
+        <LoginForm saveUser={saveUser} setNotificationMsg={setNotificationMsg} />
       ) : (
         <div>
           <h2>Blogs</h2>
@@ -140,7 +80,7 @@ function App() {
             <button onClick={handleLogout}>logout</button>
           </p>
           <Togglable buttonLabel="new blog">
-            <BlogForm addBlog={addBlog} newBlog={newBlog} handleChange={handleBlogChange} />
+            <BlogForm addBlog={addBlog} setNotificationMsg={setNotificationMsg} />
           </Togglable>
           <div className="blog-list" >
             {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
