21c53a14a74b2c2d0d86a05e1fd04cb014c8466e
Maria Tudose <maria.tudose@aalto.fi>
2020-01-10 16:25:06 +0200

    6.16 anekdootit ja backend, step1

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux'
 
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
 
-const App = () => (
-  <div>
-    <h2>Anecdotes</h2>
-    <Notification />
-    <Filter />
-    <AnecdoteList />
-    <AnecdoteForm />
-  </div>
-)
+const App = ({ initializeAnecdotes }) => {
+  useEffect(() => {
+    anecdoteService.getAll().then(a => initializeAnecdotes(a))
+  }, [])
 
-export default App
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Notification />
+      <Filter />
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default connect(null, { initializeAnecdotes })(App)
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,11 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -17,7 +9,7 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = []
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
@@ -34,6 +26,8 @@ const reducer = (state = initialState, action) => {
     case 'NEW_ANECDOTE':
       const anecdote = action.data
       return  [ ...state, anecdote ]
+    case 'INIT_ANECDOTES':
+        return action.data
     default:
       return state
   }
@@ -50,6 +44,13 @@ export const createAnecdote = (content) => {
   return {
     type: 'NEW_ANECDOTE',
     data: asObject(content)
+  }
+}
+
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes
   }
 }
 
